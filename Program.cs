9335f7c05514e5bc9e047c01999bca2ef2d6cac7
yukhimich <145339550+yukhimich@users.noreply.github.com>
2023-10-02 18:01:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+using System;
+
+class Rectangle
+{
+    private double side1;
+    private double side2;
+
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public double CalculateArea()
+    {
+        return side1 * side2;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (side1 + side2);
+    }
+
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть довжину першої сторони прямокутника: ");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть довжину другої сторони прямокутника: ");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Площа прямокутника: {rectangle.Area}");
+        Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter}");
+    }
+}
+
+using System;
+
+class Point
+{
+    private double x;
+    private double y;
+    private string name;
+
+    public Point(double x, double y, string name)
+    {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public double X
+    {
+        get { return x; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+}
+
+class Figure
+{
+    private Point[] points;
+
+    public Figure(Point p1, Point p2, Point p3)
+    {
+        points = new Point[] { p1, p2, p3 };
+    }
+
+    public Figure(Point p1, Point p2, Point p3, Point p4)
+    {
+        points = new Point[] { p1, p2, p3, p4 };
+    }
+
+    public Figure(Point p1, Point p2, Point p3, Point p4, Point p5)
+    {
+        points = new Point[] { p1, p2, p3, p4, p5 };
+    }
+
+    public double GetSideLength(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            perimeter += GetSideLength(points[i], points[i + 1]);
+        }
+        perimeter += GetSideLength(points[points.Length - 1], points[0]); // Замикання фігури
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point A = new Point(0, 0, "A");
+        Point B = new Point(4, 0, "B");
+        Point C = new Point(0, 3, "C");
+
+        Figure triangle = new Figure(A, B, C);
+        triangle.CalculatePerimeter();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab 3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab 3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4c4ca41b-990f-4d3b-a55a-0f78d93cfb67")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
